86b2ecf4899f6cdc60cb54f4094c9e013af216d7
ViniciusCarvalho94 <59082152+ViniciusCarvalho94@users.noreply.github.com>
2020-10-18 03:44:59 -0300

    organização dos documentos

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,50 +1,49 @@
 const options = {
-    dragging: false,
-    touchzoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false,
-}
+  dragging: false,
+  touchzoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
 // create map
-const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+const map = L.map("mapid", options).setView([-27.222633, -49.6455874], 15);
 
 //create and add titlelayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    ironSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+  iconUrl: "./public/images/map-marker.svg",
+  ironSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 // create and add marker
-L
-.marker([-27.222633, -49.6455874], { icon })
-.addTo(map)
+L.marker([-27.222633, -49.6455874], { icon }).addTo(map);
 
 /* image galery */
 function selectImage(event) {
-    const button = event.currentTarget
+  const button = event.currentTarget;
 
-    // remover todas as classes .active
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(removeActiveClass)
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
-    function removeActiveClass(button) {
-        button.classList.remove("active")        
-    }
-    // selecionar a image clicada
-    const image = button.children[0]
-    const imageContainer = document.querySelector(".orphanage-details > img")
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+  // selecionar a image clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
-    // atualizar o container de image
-    imageContainer.src = image.src
+  // atualizar o container de image
+  imageContainer.src = image.src;
 
-    // adicionar a classe .active para este botão
-    button.classList.add('active')
+  // adicionar a classe .active para este botão
+  button.classList.add("active");
 }
