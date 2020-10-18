5b3b9243b49bd07d545cfaa42845cc8dd7b6ebd7
ViniciusCarvalho94 <59082152+ViniciusCarvalho94@users.noreply.github.com>
2020-10-18 16:19:27 -0300

    install hbs
    hbs 4.1.1
    arquivos html para hbs

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 // importar dependencia
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 
 // iniciando do express
-const server = express();
+const server = express()
+server
 
-// criar rota
-server.get('/', () => {
-    console.log('oi')
-})
+  //utilizando os arquivos estáticos
+  .use(express.static("public"))
+
+  // configurar template engine
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'hbs')
+
+  // criar uma rota
+  .get("/", (request, response) => {
+    return response.render('index')
+  })
 
 // ligar o servidor
-server.listen(5500)
+server.listen(5500);
