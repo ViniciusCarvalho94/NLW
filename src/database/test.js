62e20415e56911aa336f663c123a9d6a331198dc
ViniciusCarvalho94 <59082152+ViniciusCarvalho94@users.noreply.github.com>
2020-10-18 22:38:01 -0300

    att 4 fix
    database.sqlite excluido.
    criada nova database.sqlite devido a um erro de não ter a informação about(que estava como description) e o dado whatsapp.
    agora a database.sqelite so tem 2 ids que são lar das meninas e lar dos meninos respectivamente.

--- src/database/test.js
+++ src/database/test.js
@@ -1,27 +1,34 @@
-const Database = require('./db');
-const saveOrphanage = require('./saveOrphanage');
+const Database = require("./db");
+const saveOrphanage = require("./saveOrphanage");
 
-Database.then(async db => {
-    // inserir dados na tabela
-    await saveOrphanage(db, {
-        lat: "-27.222633",
-        lng:  "-49.6655874",
-        name: "Lar de amor",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-        images: [
-            "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            
-            "https://images.unsplash.com/photo-1600712244180-7ef5e5c82cb5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
-        ].toString(),
-        instuctions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
-        opening_hours: "Horário de visitas Das 18h até 8h",
-        open_on_weekends: "1"
-    })
-    // consultar dados da tabela
-    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
-    console.log(selectedOrphanages)
+Database.then(async (db) => {
+  // inserir dados na tabela
+  await saveOrphanage(db, {
+    lat: "-27.222633",
+    lng: "-49.6555874",
+    name: "Lar dos meninos",
+    about:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "98989897898",
+    images: [
+      "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
-    // consultar somente 1 orfanato, pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
-    console.log(orphanage)
-})
+      "https://images.unsplash.com/photo-1600712244180-7ef5e5c82cb5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ].toString(),
+    instuctions:
+      "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0",
+  })
+  // consultar dados da tabela
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+  console.log(selectedOrphanages);
+
+  // consultar somente 1 orfanato, pelo id
+  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
+  console.log(orphanage)
+
+  // deletar dado da tabela
+  // console.log(await db.run("DELETE FROM orphanages WHERE id ='4'"))
+  // console.log(await db.run("DELETE FROM orphanages WHERE id ='5'"))
+});
