d0604bde78615547ccfd7539470f4a91d365a569
ViniciusCarvalho94 <59082152+ViniciusCarvalho94@users.noreply.github.com>
2020-10-18 22:03:08 -0300

    att 3
    test.js esta adicionando orfanatos ao database.sqlite.
    database.sqlite tem 2 orfanatos id 1 que é o lar das meninas e o id 2 que é o lar dos meninos.
    add dentro do test.js uma maneira de procurar o orfanato pelo id(pode ser alterado para qualquer dado do orfanato)

--- src/database/test.js
+++ src/database/test.js
@@ -1,12 +1,13 @@
 const Database = require('./db');
 
-Database.then(function(db) {
+Database.then(async db => {
     // inserir dados na tabela
-    db.run(`
+       /* await db.run(`
         INSERT INTO orphanages (
             lat,
             lng,
             name,
+            about,
             whatsapp,
             images,
             instructions,
@@ -14,14 +15,21 @@ Database.then(function(db) {
             open_on_weekends
         ) VALUES (
             "-27.222633",
-            "-49.6455874",
-            "Lar das meninas",
+            "-49.6555874",
+            "Lar dos meninos",
             "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+            "989809876567",
             "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
             "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
             "Horário de visitas Das 18h até 8h",
-            "1"
+            "0"
         );  
-    `)
+    `) */
     // consultar dados da tabela
+    //const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+    // console.log(selectedOrphanages)
+
+    // consultar somente 1 orfanato, pelo id
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    console.log(orphanage)
 })
