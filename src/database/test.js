30e3f32abe9ba44a3d7d90cded462c83671c71d4
ViniciusCarvalho94 <59082152+ViniciusCarvalho94@users.noreply.github.com>
2020-10-18 22:24:52 -0300

    att 4
    add saveOrphanage.js com os dados do test.js porem com modificações no VALUES aonde todos os dados são variaveis.
    add orfanato com id 3 ao database.sqlite
    test.js modificado e testado funcionando 100%

--- src/database/test.js
+++ src/database/test.js
@@ -1,35 +1,27 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
     // inserir dados na tabela
-        await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-27.222633",
-            "-49.6555874",
-            "Lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-            "989809876567",
+    await saveOrphanage(db, {
+        lat: "-27.222633",
+        lng:  "-49.6655874",
+        name: "Lar de amor",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
             "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horário de visitas Das 18h até 8h",
-            "0"
-        );  
-    `)
+            
+            "https://images.unsplash.com/photo-1600712244180-7ef5e5c82cb5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ].toString(),
+        instuctions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    })
     // consultar dados da tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
 
     // consultar somente 1 orfanato, pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
     console.log(orphanage)
 })
